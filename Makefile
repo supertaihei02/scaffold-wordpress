a9bb11fadd6a56d5a4e0da8293d2089669ba00c1
kenji nakanishi <nakanishikenji0929@gmail.com>
2017-10-25 17:06:34 +0900

    #9 Wordmoveが利用できないDBの場合を考えてDumpファイルを利用したImport, Exportは残しておく

--- Makefile
+++ Makefile
@@ -99,3 +99,22 @@ stg-push-plugins:
 
 stg-push-uploads:
 	bash tools/wordmove/move.sh staging push -u
+
+# DUMP
+import:
+	bash tools/shell-scripts/db-import.sh
+
+import-from-production:
+	bash tools/shell-scripts/db-import.sh $(PRODUCTION_DOMAIN) $(LOCAL_DOMAIN)
+
+import-from-staging:
+	bash tools/shell-scripts/db-import.sh $(STAGING_DOMAIN) $(LOCAL_DOMAIN)
+
+export:
+	bash tools/shell-scripts/db-export.sh
+
+export-to-production:
+	bash tools/shell-scripts/db-export.sh $(PRODUCTION_DOMAIN) $(LOCAL_DOMAIN)
+
+export-to-staging:
+	bash tools/shell-scripts/db-export.sh $(STAGING_DOMAIN) $(LOCAL_DOMAIN)
---
+++ tools/shell-scripts/db-export.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#####################################
+# 現在のデータベースの情報をGit管理します
+#####################################
+
+cd $(dirname $0)
+ENV_FILE=../../.env
+
+if [ ! -f $ENV_FILE ]; then
+    echo "FILE '$ENV_FILE' does not exist."
+    exit;
+fi
+
+source $ENV_FILE
+DUMP_FILE=../../$DUMP_PATH
+WK_DUMP_FILE=../../$DUMP_PATH.wk.sql
+WK_DUMP_CONTAINER_PATH=$DUMP_CONTAINER_PATH.wk.sql
+
+# 1回バックアップ用にそのままダンプする
+docker exec $MYSQL_CONTAINER /bin/bash -c "mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $DUMP_CONTAINER_PATH && cp -f $DUMP_CONTAINER_PATH $WK_DUMP_CONTAINER_PATH" \
+     && echo "- Export to '$DUMP_PATH'" && git add -f $DUMP_FILE
+
+# ドメインの変換処理を行う
+# ARGS
+case $# in
+    1 ) DOMAIN=$1; LOCAL='localhost'; break;;
+    2 ) DOMAIN=$1; LOCAL=$2; break;;
+    * ) rm $WK_DUMP_FILE; exit 0;;
+esac
+docker exec $WORDPRESS_CONTAINER /bin/bash -c "bash /var/www/tools/shell-scripts/search-replace.sh $LOCAL $DOMAIN"
+echo "- Convert $LOCAL to $DOMAIN"
+
+# 変換後のDBをダンプする
+docker exec $MYSQL_CONTAINER /bin/bash -c "mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $DUMP_CONTAINER_PATH;" \
+    && echo "- Export to '$DUMP_PATH'" && git add -f $DUMP_FILE
+    
+# コピーしておいたDB情報を再インポートする
+docker exec $MYSQL_CONTAINER /bin/bash -c "mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $WK_DUMP_CONTAINER_PATH;" \
+    && echo "- Import from '$WK_DUMP_FILE'"
+
+rm $WK_DUMP_FILE
