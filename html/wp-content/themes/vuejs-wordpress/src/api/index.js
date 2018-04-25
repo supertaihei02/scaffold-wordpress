0f7c8e0c443efa9ca28f311a05565ce382691bac
yokosuka <yokosuka@framelunch.jp>
2018-04-25 14:06:49 +0900

    相対パス化（共通関数にできるけど面倒）

--- html/wp-content/themes/vuejs-wordpress/src/api/index.js
+++ html/wp-content/themes/vuejs-wordpress/src/api/index.js
@@ -12,6 +12,16 @@ export default {
   getPages (cb) {
     axios.get(window.SETTINGS.API_BASE_PATH + 'pages?per_page=10')
       .then(response => {
+        if( location.origin === undefined ){
+          location.origin = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
+        }
+        for(var i in response.data) {
+          let page = response.data[i]
+          if (!page.link.indexOf(location.origin)) { //same domain
+            page.link = page.link.split(location.origin).join('')
+          }
+          response.data[i] = page
+        }
         cb(response.data)
       })
       .catch(e => {
@@ -23,6 +33,12 @@ export default {
     if (_.isNull(id) || !_.isNumber(id)) return false
     axios.get(window.SETTINGS.API_BASE_PATH + 'pages/'+id)
       .then(response => {
+        if( location.origin === undefined ){
+          location.origin = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
+        }
+        if (!response.data.link.indexOf(location.origin)) { //same domain
+          response.data.link = response.data.link.split(location.origin).join('')
+        }
         cb(response.data)
       })
       .catch(e => {
@@ -35,6 +51,16 @@ export default {
     
     axios.get(window.SETTINGS.API_BASE_PATH + 'posts?per_page='+limit)
       .then(response => {
+        if( location.origin === undefined ){
+          location.origin = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
+        }
+        for(var i in response.data) {
+          let post = response.data[i]
+          if (!post.link.indexOf(location.origin)) { //same domain
+            post.link = post.link.split(location.origin).join('')
+          }
+          response.data[i] = post
+        }
         cb(response.data)
       })
       .catch(e => {
